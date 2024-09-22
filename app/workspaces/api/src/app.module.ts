5bb23793037764e213e52a308a85588b74bc241c
Oleg Chepelev <yorkblansh@gmail.com>
2024-09-22 13:01:55 +0300

    rf: app.module

--- app/workspaces/api/src/app.module.ts
+++ app/workspaces/api/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from '../prisma/prisma.module'
 import { TaskModule } from './task/task.module'
@@ -17,7 +16,6 @@ import { LoggerModule } from 'nestjs-pino'
 		FileUploadModule,
 		LoggerModule.forRoot(),
 	],
-	controllers: [AppController],
 	providers: [AppService],
 })
 export class AppModule {}
